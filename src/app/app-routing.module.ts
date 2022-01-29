463896c478d18ab4da077f70f4aac236a49babd2
aledguedes <alexandredguedes@gmail.com>
2022-01-29 09:57:45 -0300

    Footer, Main, Navbar more pages of Links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompanyComponent } from './components/company/company.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { MainComponent } from './components/main/main.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'company', component:CompanyComponent },
+  { path: 'contact', component: ContactComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { CompanyComponent } from './components/company/company.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    MainComponent,
+    ProjectsComponent,
+    ContactComponent,
+    CompanyComponent
   ],
   imports: [
     BrowserModule,
